495aeddb0533a9c4b18bf4b22d99aabbfee082b0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-30 00:40:00 +0530

    Sidebar completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <></>
+    <main className='bg-gray-100 min-h-screen'>
+      <Header />
+    </main>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+      <h2>Dashboard</h2>
+      <h2>Welcome back, Rishi </h2>
+    </div>
+  )
+}
+
+export default Header

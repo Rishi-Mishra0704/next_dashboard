ec12ede129bb77dde06d69d690ae27e96f1ddb72
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-30 00:56:55 +0530

    add data cards and make header responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import TopCard from '@/components/TopCard'
 
 export default function Home() {
   return (
     <main className='bg-gray-100 min-h-screen'>
       <Header />
+      <TopCard />
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <div className='flex justify-between px-4 pt-4'>
-      <h2>Dashboard</h2>
-      <h2>Welcome back, Rishi </h2>
+    <div className='px-4 pt-4 sm:flex sm:justify-between md:px-8 lg:px-16 xl:px-24'>
+      <h2 className='text-lg sm:text-xl lg:text-2xl xl:text-3xl font-semibold'>
+        Dashboard
+      </h2>
+      <h2 className='text-sm sm:text-base lg:text-lg xl:text-xl'>
+        Welcome back, Rishi
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
